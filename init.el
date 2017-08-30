e6ab843e681520dc10726eda9d303e89f7cd22f8
Mark Herman <mark.herman@sonian.net>
2017-08-30 10:58:27 -0500

    Fixes JSX attr indent and trailing whitespace

--- init.el
+++ init.el
@@ -196,20 +196,20 @@
 
 ;; adjust indents for web-mode to 2 spaces
 (defun my-web-mode-hook ()
-  "Hooks for Web mode. Adjust indents"
+  "My customizations for Web mode"
   ;;; http://web-mode.org/
+  
+  ;;; Set indents
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
-;; for better jsx syntax-highlighting in web-mode
-;; - courtesy of Patrick @halbtuerke
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-    (let ((web-mode-enable-part-face nil))
-      ad-do-it)
-    ad-do-it))
+(add-hook 'before-save-hook 'my-prog-nuke-trailing-whitespace)
+(defun my-prog-nuke-trailing-whitespace ()
+  (when (derived-mode-p 'prog-mode)
+    (delete-trailing-whitespace)))
 
 (tool-bar-mode 0)
 
